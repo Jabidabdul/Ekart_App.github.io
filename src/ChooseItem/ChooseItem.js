f16ed6c8814e5fb7f3811b54bdf5aca16333f287
Jabidabdul <72267207+Jabidabdul@users.noreply.github.com>
2021-09-10 11:47:23 +0530

    Update ChooseItem.js

--- src/ChooseItem/ChooseItem.js
+++ src/ChooseItem/ChooseItem.js
@@ -1,4 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext } from "react";import React, { useContext } from "react";
+import "./ChooseItem.css";
+// import { CartContext } from "../../context/cartContext";
+
+function ChooseItem({ setAdd, items }) {
+    //let { items } = props;
+    let list = [{}];
+    //console.log(items)
+    function addToCart() {
+
+        list.push(items)
+        // console.log(list)
+        setAdd(list)
+
+
+    }
+    // let [cartItem, setCartItem] = React.useContext(CartContext);
+    return (
+        <div className="item" >
+            <div className="img-wrapper1" >
+                <img src={items.searchImage} alt="product-img" />
+            </div>
+            <div className="content-wrapper1">
+                <h3 className="brand-name1" >{items.brand}</h3>
+                <h4 className="brand-desc1" > {items.additionalInfo}</h4>
+                <h5>
+                    <span className="b-d-price1">Rs. {items.price}</span>
+                    {items.discountDisplayLabel ? (
+                        <span className="b-price1">Rs. {items.mrp}</span>
+                    ) : null}
+                    <span className="b-discount1">{items.discountDisplayLabel}</span>
+                </h5>
+                <button className="add-to-cart-btn1" onClick={addToCart}>
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+        // <div>hi</div>
+    )
+};
+
+export default ChooseItem;
+
 import "./ChooseItem.css";
 // import { CartContext } from "../../context/cartContext";
 
