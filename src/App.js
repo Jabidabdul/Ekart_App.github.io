6bf16550a8918b34b3aa04964301b40c518b7601
Jabidabdul <72267207+Jabidabdul@users.noreply.github.com>
2021-09-10 14:53:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 import Cart from './components/Cart/Cart';
 import Lists from './components/Lists/Lists';
 function App() {
+
+  let [add, setAdd] = React.useState([{}]);
   return (
 
     <div className="App">
       <header className="App-header">
-
+        {console.log(add)}
         <div className="right-header">
-          <Cart />
+          <Cart addToCart={add} />
         </div>
       </header>
       <div className="main-content">
-        <Lists />
+        <Lists setAdd={setAdd} />
       </div>
 
     </div>
