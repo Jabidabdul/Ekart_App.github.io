bd2660c542df1360ba06f975ac3dde020d37a167
Jabidabdul <72267207+Jabidabdul@users.noreply.github.com>
2021-09-10 11:48:10 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ import "./Cart.css";
 
 function Cart(props) {
     // let [cartItem, setCartItem] = React.useContext(CartContext);
-
+    // console.log(props)
     return (
         <div className="cart-wrapper">
             <button >Cart</button>
